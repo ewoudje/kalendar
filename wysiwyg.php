546e7e66c78d48c31bb5bac116129434c7cf651c
BlackFoxGames <sachavossie@gmail.com>
2020-01-28 09:18:58 +0100

    wysiwyg

---
+++ wysiwyg.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="robots" content="noindex, nofollow">
+    <title>UI color picker</title>
+    <script src="lib/ckeditor_4.13/ckeditor/ckeditor.js"></script>
+</head>
+
+<body>
+    <textarea id="editor1"></textarea>
+    <script>
+	CKEDITOR.replace( 'editor1', '' );
+    </script>
+</body>
+
+</html>
+
+</html>
